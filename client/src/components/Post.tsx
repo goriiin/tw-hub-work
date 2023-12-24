30fe00a13c33135d0429f9de06e305718bc10123
VadimKochnev <124500780+sendemy@users.noreply.github.com>
2023-12-25 00:48:03 +0300

    likes & dislikes added

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -2,6 +2,7 @@
 
 import { formatDate } from '@/actions/index'
 import Link from 'next/link'
+import { BiSolidDislike, BiSolidLike } from 'react-icons/bi'
 
 type Props = {
 	postId: number
@@ -27,13 +28,29 @@ export default function Post({
 	dislikesCount,
 }: Props) {
 	return (
-		<div className='flex flex-col gap-4 w-full max-w-[600px] bg-blue-200 rounded-xl p-2'>
-			<Link href={`/posts/${postId}`}>{text}</Link>
-			<div className='flex flex-row gap-8'>
+		<div className='flex flex-col w-full max-w-[600px] bg-blue-200 rounded-xl p-2'>
+			<div className='mb-4'>
+				<Link href={`/posts/${postId}`}>{text}</Link>
+			</div>
+			<div className='flex flex-row mb-1'>
 				<Link className='ml-0 mr-auto' href={`/user/${userId}`}>
 					@{username}
 				</Link>
 				<span className='ml-auto mr-0'>{formatDate(date)}</span>
+			</div>
+			<div className='flex flex-row gap-6'>
+				<div className='flex flex-row items-center gap-1'>
+					<div className={isLiked == 'true' ? 'text-green-600' : ''}>
+						<BiSolidLike />
+					</div>
+					{likesCount}
+				</div>
+				<div className='flex flex-row items-center gap-1'>
+					<div className={isDisliked == 'true' ? 'text-red-600' : ''}>
+						<BiSolidDislike />
+					</div>
+					{dislikesCount}
+				</div>
 			</div>
 		</div>
 	)
